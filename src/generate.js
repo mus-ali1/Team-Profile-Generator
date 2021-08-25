2387962d7d64bc6bc0a6573125b52f2c0eb8180e
Mustafa Ali <mustafamire@gmail.com>
2021-08-25 12:04:25 +0100

    Add: Engineer Template

--- src/generate.js
+++ src/generate.js
@@ -14,3 +14,20 @@ const internTemplate = (internData) => {
 
     return template;
 }
+
+const engineerTemplate = (engineerData) => {
+    const { name, id, email, role, github } = engineerData;
+
+    const template = `
+    <div class="card">
+        <header>
+            <h2 class="name header">${name}</h2>
+            <h3 class="role header">${role}</h3>
+        </header>
+        <p class="details">Employee ID: ${id}</p>
+        <p class="details">Email: <a href="mailto:">${email}</a></p>
+        <p class="details">Github: <a href="https://github.com/${github}" target="_blank">${github}</a></p>
+    </div>`
+
+    return template;
+}
