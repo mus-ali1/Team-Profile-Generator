c91be4262d2fd49b7d2bb0abab0300e992068f12
Mustafa Ali <mustafamire@gmail.com>
2021-08-25 12:25:18 +0100

    Add: Default HTML for employee

--- src/generate.js
+++ src/generate.js
@@ -67,3 +67,29 @@ const createHTML = (employeeData) => {
                 break;
         }
     }
+
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <header>
+            <h1 class="header">My Team</h1>
+        </header>
+    
+        <main>
+            <div class="container">
+                ${employeeHTML}                
+            </div>
+        </main>
+        
+    </body>
+    </html>`
+}
+
+module.exports = createHTML;
