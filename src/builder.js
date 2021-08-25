f86330a2d4c05f38a7c24e7883dae26050c3a30d
Mustafa Ali <mustafamire@gmail.com>
2021-08-25 12:31:39 +0100

    Add: Import Dependencies

---
+++ src/builder.js
@@ -0,0 +1,8 @@
+// Import 
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const Engineer = require("../lib/engineer.js");
+const Manager = require("../lib/manager.js");
+const Intern = require("../lib/intern.js");
+const generateHTML = require("./generate.js");
