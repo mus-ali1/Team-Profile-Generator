43aa2e0969a3bd41de33bd780d10f3356893c76d
Mustafa Ali <mustafamire@gmail.com>
2021-08-26 13:58:24 +0100

    Add:  Employee test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,26 @@
+const Employee = require('../lib/employee');
+
+//name,email,id,role validation
+
+const employeeTest = new Employee("John", "John.Simpson90@gmail.com", 3, "Employee");
+
+
+describe('Employee', () => {
+    it('has a name', () => {
+        expect(employeeTest.name).toEqual(expect.any(String))
+        expect(employeeTest.name.length).toBeGreaterThan(2)
+    })
+
+    it('has an email id', () => {
+        expect(employeeTest.email).toEqual(expect.stringContaining('@'))
+    })
+
+    it('has an id that is a num', () => {
+        expect(employeeTest.id).toEqual(expect.any(Number))
+    })
+
+    it('has a role of employee', () => {
+        expect(employeeTest.role).toBe('Employee')
+    })
+
+})
