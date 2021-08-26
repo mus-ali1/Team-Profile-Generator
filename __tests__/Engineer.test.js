0f91cf7baafcbd46a1ae77512206df649a888dc2
Mustafa Ali <mustafamire@gmail.com>
2021-08-26 14:04:11 +0100

    Add: Engineer Test

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,34 @@
+const Engineer = require('../lib/Engineer');
+
+//name, email, id, role and github user name validation for Engineer
+
+const engineerTest = new Engineer('Mustafa', 'mustafamire@gmail.com', 2, 'Engineer', 'musali1')
+
+describe("Engineer", () => {
+    it('has a name', () => {
+        expect(engineerTest.name).toEqual(expect.any(String))
+        expect(engineerTest.name.length).toBeGreaterThan(2)
+
+    })
+    it('has an email id', () => {
+        expect(engineerTest.email).toEqual(expect.stringContaining('@'))
+    })
+
+    it('has an id that is a num', () => {
+        expect(engineerTest.id).toEqual(expect.any(Number))
+    })
+
+    it('has a role of engineer', () => {
+        expect(engineerTest.role).toBe('Engineer')
+    })
+
+    it('has a github username', () => {
+        keys = Object.keys(engineerTest)
+        optionKey = keys[4]
+        expect(optionKey).toBe('gitHub')
+        expect(engineerTest.gitHub).toEqual(expect.any(String))
+    })
+
+
+})
+
