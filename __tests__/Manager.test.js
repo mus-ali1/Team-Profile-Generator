946b25b98dc3fdc0f955f3256f88d0ea3bb173db
Mustafa Ali <mustafamire@gmail.com>
2021-08-29 19:36:32 +0100

    Add: changes to Manager testing

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,30 +1,27 @@
 const Manager = require('../lib/manager');
 
 //name, email, id, role and school  name validation for manager
-const managerTest = new Manager('mustafa', 'mustafa@gmail.com', 1, 'Manager', 101)
+const managerTest = new Manager('mustafa', 1, "mustafamire@gmail.com", 'Manager', 101)
 
 describe('Manager', () => {
     it('has a name', () => {
         expect(managerTest.name).toEqual(expect.any(String))
         expect(managerTest.name.length).toBeGreaterThan(2)
-
+    })
+    it('has an id that is a num', () => {
+        expect(managerTest.id).toEqual(expect.any(Number))
     })
     it('has an email id', () => {
         expect(managerTest.email).toEqual(expect.stringContaining('@'))
     })
-
-    it('has an id that is a num', () => {
-        expect(managerTest.id).toEqual(expect.any(Number))
-    })
-
-    it('has a role of engineer', () => {
-        expect(managerTest.role).toBe('Manager')
-    })
-
     it('has an office number', () => {
         keys = Object.keys(managerTest)
         optionKey = keys[4]
         expect(optionKey).toBe('office')
         expect(managerTest.office).toEqual(expect.any(Number))
     })
+    it('has a role of engineer', () => {
+        expect(managerTest.getRole()).toBe('Manager')
+    }
+    )
 })
